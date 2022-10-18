3b8fe0f5ec72b3be68c0c83d2eaa59099d7fddd8
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-18 15:51:13 +1100

    commit tree 29758fad5f602fb0898af9602275ff92acd2902f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+        
+        return {firstName,familyName,title,payPerHour, timeInEvents : [], timeOutEvents: []}
+}
+
+
+function createEmployeeRecords(records){
+    let employeeRecords = []
+    for (let record of records){
+        employeeRecords.push(createEmployeeRecord(record))
+    }
+    return employeeRecords;
+}
+
+function createTimeInEvent(obj, event){
+   let [date,hour] = event.split(' ');
+    hour = parseInt(hour);
+    obj.timeInEvents.push({
+        type : 'TimeIn',
+        date,
+        hour
+        
+    })
+    return obj
+}
+
+let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
+let updatedBpRecord = createTimeOutEvent(bpRecord, "2015-02-28 1700")
+let newEvent = updatedBpRecord.timeOutEvents[0]
+console.log(updatedBpRecord)
+
+function createTimeOutEvent(obj, timeStamp){
+    let  [date,hour] = timeStamp.split(' ');
+    hour = parseInt(hour);
+    obj.timeOutEvents.push({
+        type : 'TimeOut',
+        date,
+        hour,
+    })
+    return obj;
+}
+
